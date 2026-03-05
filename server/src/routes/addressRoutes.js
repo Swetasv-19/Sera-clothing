78fc9e9a121c4fe79746e7f06ef6a33ba6e48ec8
Sharwin-sriram <shreeramtooo@gmail.com>
2026-03-06 03:12:50 +0530

    update addressRoutes

--- server/src/routes/addressRoutes.js
+++ server/src/routes/addressRoutes.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
-const { getAddresses, addAddress, updateAddress, deleteAddress } = require('../controller/addressController');
+const {
+  getAddresses,
+  addAddress,
+  updateAddress,
+  deleteAddress,
+  setDefaultAddress,
+  getAddressById
+} = require('../controller/addressController');
 
 const router = express.Router();
 
 router.use(protect);
 
+// GET /api/addresses - Get all user addresses
 router.get('/', getAddresses);
+
+// POST /api/addresses - Add new address
 router.post('/', addAddress);
+
+// GET /api/addresses/:id - Get specific address
+router.get('/:id', getAddressById);
+
+// PUT /api/addresses/:id - Update address
 router.put('/:id', updateAddress);
+
+// PUT /api/addresses/:id/default - Set as default address
+router.put('/:id/default', setDefaultAddress);
+
+// DELETE /api/addresses/:id - Delete address
 router.delete('/:id', deleteAddress);
 
 module.exports = router;
